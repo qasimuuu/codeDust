c1ad62eb046850b8b016e2141894c4a545278a7d
qasimuuu <91064522+qasimuuu@users.noreply.github.com>
2022-01-28 17:19:06 +0300

    Add files via upload

---
+++ MOUSEPRESS_2022_01_28_12_53_20/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,50,60);
+  noStroke()
+  fill(0,255,50);
+  circle(mouseX,mouseX,60);
+  
+}
+function mouseprssed(){
+  
+}
