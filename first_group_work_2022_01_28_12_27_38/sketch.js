b9b5a4229c2ceac79156b7fab71169345161ad38
qasimuuu <91064522+qasimuuu@users.noreply.github.com>
2022-01-28 16:44:10 +0300

    Add files via upload

---
+++ first_group_work_2022_01_28_12_27_38/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,220,0,600);
+  fill(00);
+  ellipse(100,80,80,100);
+  fill(100,200,0);
+  rect(200, 200, 120, 60);
+  
+}
